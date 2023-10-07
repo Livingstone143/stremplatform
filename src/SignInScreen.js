7815563f2c63fd8897b617c1cbd8c52dc2df00d4
Livingstone143 <paramalla2004@gmail.com>
2023-10-08 02:32:50 +0530

    signin functionality -1

--- src/SignInScreen.js
+++ src/SignInScreen.js
@@ -26,7 +26,7 @@ function SignInScreen() {
         <h1>Sign In</h1>
         <input placeholder='Email' type="email"></input>
         <input placeholder='Password' type='password'></input>
-        <button type="submit" onClick={signIn}>Sign In</button>
+        <button type="submit" onSubmit={signIn}>Sign In</button>
         <div className='remember'>
           <input type="checkbox" name='cb'></input>
           <label for='cd'>Remember me</label>
